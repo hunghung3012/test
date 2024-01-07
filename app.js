fe3e87d00aa2de725dfa29077592848f11ee1ec9
hungvn <vonhathung3012@gmail.com>
2024-01-08 00:40:09 +0700

    test

---
+++ app.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    fetch('https://api.qrserver.com/v1/read-qr-code/?fileurl=[https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=123]')
+   
+    .then((reponse) => {
+        console.log(reponse);
+    })
+});
